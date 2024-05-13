87abc328e664619fe80f7c5d0c9f2fa276cc299b
Klinger Oliveira <60447413+klingeroliveira@users.noreply.github.com>
2024-05-13 19:56:21 -0300

    Add files via upload

---
+++ PassIn.Api/Controllers/CheckInController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.CheckIns;
+using PassIn.Communication.Responses;
+
+namespace PassIn.Api.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckInController : ControllerBase
+    {
+        [HttpPost]
+        [Route("{attendeeId}")]
+        [ProducesResponseType(typeof(ResponseRegisteredJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status409Conflict)]
+        public IActionResult Checkin([FromRoute] Guid attendeeId)
+        {
+
+            var useCase = new DoAttendeeCheckIn();
+
+            var response = useCase.Execute(attendeeId);
+
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ PassIn.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using System.Net;
+
+namespace PassIn.Api.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = context.Exception is PassInException;
+            if (result)
+            {
+                HandleProjectException(context);
+            }
+            else
+            {
+                ThrowUnknowError(context);
+            }
+        }
+
+        private void HandleProjectException(ExceptionContext context)
+        {
+            if (context.Exception is NotFoundExcpetion)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new NotFoundObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+            else if (context.Exception is ErroOnValidationException)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Result = new BadRequestObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+            else if (context.Exception is ConflictException)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status409Conflict;
+                context.Result = new ConflictObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+
+        }
+
+        private void ThrowUnknowError(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = ((int)HttpStatusCode.InternalServerError);
+            context.Result = new ObjectResult(new ResponseErrorJson("Erro desconhecido"));
+        }
+    }
+
+     
+}
